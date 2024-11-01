bb554361f1c93cb0649fe40e5f1b629923402240
Jure Topolak <jure.topolak@gmail.com>
2024-11-01 17:53:03 +0100

    Install nuxt/eslint to generate nuxt-specific rules and use antfu/eslint-config as custom config presets.

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import antfu from '@antfu/eslint-config'
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  antfu({
+    // ...@antfu/eslint-config options
+    formatters: {
+      /**
+       * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
+       * By default uses Prettier
+       */
+      css: true,
+      /**
+       * Format HTML files
+       * By default uses Prettier
+       */
+      html: true,
+      /**
+       * Format Markdown files
+       * Supports Prettier and dprint
+       * By default uses Prettier
+       */
+      markdown: 'prettier',
+    },
+  }),
+  // ...your other rules
+)
