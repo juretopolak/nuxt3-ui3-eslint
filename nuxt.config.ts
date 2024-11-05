6fd4f55ccc5a2938210e3c6992d530d58ed194b6
Jure Topolak <jure.topolak@gmail.com>
2024-11-05 22:24:58 +0100

    Enable compatibilityVersion: 4 feature.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   modules: ['@nuxt/eslint', '@nuxt/ui'],
   devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
   compatibilityDate: '2024-04-03',
   eslint: {
     config: {
