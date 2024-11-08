953594573e3f5f8276e96eabaff3ef57d2eba957
Jure Topolak <jure.topolak@gmail.com>
2024-11-09 00:08:48 +0100

    Install nuxthub/core and wrangler.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
-  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxthub/core'],
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
